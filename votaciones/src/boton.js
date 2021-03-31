05ea51e1ff0ac02dd3c1903304d48c1ab0bc426f
drandredev <drandredev@drandredev.localdomain>
2021-03-31 18:55:20 -0500

    Punto 1

--- votaciones/src/boton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Boton = (props) => {
-    return(
-        <button className="button_vote" onClick={props.handleClick}>{props.text}</button>
-    )
-}
-
-export default Boton
--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -1,35 +1,15 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import Boton from './boton.js';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-
+  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const ClickGood = () => {
-    setGood( good + 1 ) 
-  }
-  const ClickNeutral = () => {
-    setNeutral( neutral + 1 )
-
-  }
-  const ClickBad = () => {
-    setBad( bad + 1 )
-
-  }
-
   return (
     <div>
-      <h1 className="title">Give Feedback</h1>
-      <div className="buttons">
-        <Boton handleClick={ClickGood} text="good"/>
-        <Boton handleClick={ClickNeutral} text="neutral"/>
-        <Boton handleClick={ClickBad} text="bad"/>
-      </div>
-      
-
+      code here
     </div>
   )
 }
