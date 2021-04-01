62f2829b35440d929e9da11710e5a2602fa56792
drandredev <drandredev@drandredev.localdomain>
2021-03-31 19:05:06 -0500

    Punto 2.2

--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -28,9 +28,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all </p>
-      <p>average </p>
-      <p>positive </p>
+      <p>all  {good + neutral + bad}</p>
+      <p>average { (good - bad)/(good + neutral + bad) }</p>
+      <p>positive { (good / (good + neutral + bad))*100 }%</p>
     </div>
   )
 }
