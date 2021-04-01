ef69a8bf0f4a77adf1f83ddfc18ec47975d7f2d6
drandredev <drandredev@drandredev.localdomain>
2021-03-31 19:00:50 -0500

    Punto 2.1

--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -2,14 +2,35 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const ClickGood = () => {
+    setGood( good + 1 )
+  }
+  const ClickNeutral = () => {
+    setNeutral( neutral + 1 )
+  }
+  const ClickBad = () => {
+    setBad( bad + 1 )
+  }
+
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={ClickGood}>good</button>
+      <button onClick={ClickNeutral}>neutral</button>
+      <button onClick={ClickBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all </p>
+      <p>average </p>
+      <p>positive </p>
     </div>
   )
 }
